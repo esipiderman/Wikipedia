8ea40d14ad87db6cf56b5439e1f06f924f7f8ad7
esipiderman <sobiesipiderman@gmail.com>
2022-05-29 23:44:02 +0430

    add bottom navigation

--- app/src/main/java/com/example/wikipedia/MainActivity.kt
+++ app/src/main/java/com/example/wikipedia/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.example.wikipedia.databinding.ActivityMainBinding
+import com.example.wikipedia.fragments.FragmentExplore
+import com.example.wikipedia.fragments.FragmentProfile
+import com.example.wikipedia.fragments.FragmentTrend
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         //-------------SET TOOL BAR------------
         setSupportActionBar(binding.toolbarMain)
 
+        //--------------SET NAVIGATION DRAWER-----------
         val actionBarDrawerToggle = ActionBarDrawerToggle(
             this,
             binding.drawerMain,
@@ -27,12 +32,13 @@ class MainActivity : AppCompatActivity() {
         actionBarDrawerToggle.syncState()
         binding.drawerMain.addDrawerListener( actionBarDrawerToggle )
 
+        //--------------ITEMS OF NAVIGATION DRAWER------------
         binding.navigationViewMain.setNavigationItemSelectedListener {
 
             when (it.itemId){
                 R.id.menu_writer ->{
 
-                    binding.drawerMain.closeDrawer(GravityCompat.START)
+                    binding.drawerMain.closeDrawer(GravityCompat.END)
                 }
 
                 R.id.menu_photographer ->{
@@ -66,5 +72,42 @@ class MainActivity : AppCompatActivity() {
 
             true
         }
+
+        //---------------SET BOTTOM NAVIGATION------------
+        firstRun()
+
+        //--------------ITEMS OF NAVIGATION BOTTOM-------------
+        binding.bottomNavigationMain.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.menu_explore ->{
+                    replaceFragment(FragmentExplore())
+                }
+
+                R.id.menu_trend ->{
+                    replaceFragment(FragmentTrend())
+                }
+
+                R.id.menu_profile ->{
+                    replaceFragment(FragmentProfile())
+                }
+
+            }
+
+            true
+        }
+        binding.bottomNavigationMain.setOnItemReselectedListener {  }
+    }
+
+    fun replaceFragment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_main_container, fragment)
+        transaction.commit()
+    }
+
+    fun firstRun(){
+        replaceFragment(FragmentExplore())
+        binding.bottomNavigationMain.selectedItemId = R.id.menu_explore
     }
 }
