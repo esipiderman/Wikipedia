0690f6a042b7d0d8e36ffeb88b02dcd5dba84bb1
esipiderman <sobiesipiderman@gmail.com>
2022-05-30 07:18:47 +0430

    add recycler to explore fragment

---
+++ app/src/main/java/com/example/wikipedia/adapter/ExploreAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.wikipedia.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.wikipedia.data.ItemPost
+import com.example.wikipedia.databinding.ItemExploreBinding
+
+class ExploreAdapter(private val data:ArrayList<ItemPost>) :RecyclerView.Adapter<ExploreAdapter.ExploreViewHolder>() {
+    lateinit var binding:ItemExploreBinding
+
+    inner class ExploreViewHolder(itemView :View) : RecyclerView.ViewHolder(itemView){
+
+        fun bindData(itemPost : ItemPost){
+
+            Glide
+                .with(itemView.context)
+                .load(itemPost.imgUrl)
+                .into(binding.imgExplore)
+
+            binding.txtExploreTitle.text = itemPost.txtTitle
+            binding.txtExploreSubtitle.text = itemPost.txtSubTitle
+            binding.txtExploreDetail.text = itemPost.txtDetail
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExploreViewHolder {
+        binding = ItemExploreBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ExploreViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ExploreViewHolder, position: Int) {
+        holder.bindData(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
---
+++ app/src/main/java/com/example/wikipedia/data/ItemPost.kt
@@ -0,0 +1,10 @@
+package com.example.wikipedia.data
+
+data class ItemPost(
+    val imgUrl: String,
+    val txtTitle: String,
+    val txtSubTitle: String,
+    val txtDetail: String,
+    val isTrend: Boolean,
+    val insight: String
+)
